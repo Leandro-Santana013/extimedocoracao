751ede884bb131ebf5184692dcefa865550ec882
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-16 15:29:38 -0300

    extimedocoracao

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = (ConsoleColor)0;
+Console.BackgroundColor = (ConsoleColor)12;
+Console.WriteLine("São Paulo");
+Console.ResetColor();
